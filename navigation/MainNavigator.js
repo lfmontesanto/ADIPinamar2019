7025e6c9ac58b40f8697220273cf2b6f437ca6db
Facundo Montesanto <leandro.montesanto@wolox.com.ar>
2019-04-26 00:39:09 -0300

    removed logs and main navigator conflicts2

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,16 +1,10 @@
 import { createStackNavigator } from "react-navigation";
 
-<<<<<<< HEAD
-import LoginScreen from '../screens/LoginScreen';
-import HomeTabs from './HomeTabs';
-import RegisterScreen from '../screens/RegisterScreen';
-=======
 import HomeTabs from "./HomeTabs";
 import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import ShowScreen from "../screens/ShowScreen";
 import ReviewScreen from "../screens/ReviewScreen";
->>>>>>> 1090b93777fa7ec122e8ba3c35b41085f0c877ae
 
 const HomeStack = createStackNavigator({
   Login: LoginScreen,
